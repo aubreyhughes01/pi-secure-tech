5e90e92fdd9669bac4788493f4f69babbe491fdc
Aubrey Hughes <aubreyhughes01@gmail.com>
2024-09-14 18:06:35 -0400

    UPDATED servo controller with lock, unlock, and custom angle functions

--- pi_files/servo_controller.py
+++ pi_files/servo_controller.py
@@ -6,16 +6,15 @@ from time import sleep
 servo = AngularServo(14, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
 
 # values need to be figured through testing
-'''
-UNLOCK = 15
-LOCK = 30
-'''
+UNLOCK = 90
+LOCK = 0
+
 
 # functions to set the servo angle
 def set_angle(angle):
     servo.angle = angle
     sleep(1)
-'''
+
 def unlock():
     servo.angle = UNLOCK
     sleep(1)
@@ -23,13 +22,20 @@ def unlock():
 def lock():
     servo.angle = LOCK
     sleep(1)
-'''
+
 
 # main program loop
 try:
     while True:
-        angle = int(input("Enter angle (0 to 180): "))  # user input for angle
-        set_angle(angle)    # set servo to entered angle
+        angle = input("Waiting for command: ")  # user input unlock, lock, or custom
+
+        if angle == 'u':
+            unlock()
+        elif angle == 'l':
+            lock()
+        else:
+            set_angle(int(angle))
+            
 
 except KeyboardInterrupt:
     print("Program stopped by user")
