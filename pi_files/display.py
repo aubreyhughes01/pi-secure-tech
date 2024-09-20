496c9d8e4ebd24bc29892eec6273b1a4e0c9c69e
Aubrey Hughes <aubreyhughes01@gmail.com>
2024-09-20 16:38:55 -0400

    UPDATED display with pin pad

--- pi_files/display.py
+++ pi_files/display.py
@@ -1,5 +1,15 @@
-import tkinter
-m = tkinter.Tk()
+from tkinter import *
+root = Tk()
+root.title("Access Pad")
 
+btn1 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="1").grid(row=1,column=0)
+btn2 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="2").grid(row=1,column=1)
+btn3 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="3").grid(row=1,column=2)
+btn4 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="4").grid(row=2,column=0)
+btn5 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="5").grid(row=2,column=1)
+btn6 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="6").grid(row=2,column=2)
+btn7 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="7").grid(row=3,column=0)
+btn8 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="8").grid(row=3,column=1)
+btn9 = Button(root,padx=16,pady=16,bg="black",bd=15,fg="white",font=("times new roman",30,"bold"),text="9").grid(row=3,column=2)
 
-m.mainloop()
+root.mainloop()
