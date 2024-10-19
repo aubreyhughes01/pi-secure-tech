0a72cd44a74e458bca06ddcac6407c25aa23481b
Aubrey Hughes <aubreyhughes01@gmail.com>
2024-10-19 17:12:31 -0400

    UPDATED display to incorporate PWM functionality and FIXED data persistence issue

--- pi_files/Hardware_PWM/servo_controller.py
+++ pi_files/Hardware_PWM/servo_controller.py
@@ -5,9 +5,10 @@ kit = ServoKit(channels=16)
 # main program loop
 try:
     while True:
-        
+
         direction = int(input("Waiting for input: "))
 
+        kit.servo[0].set_pulse_width_range(500, 2500)
         kit.servo[0].angle = direction     
 
 except KeyboardInterrupt:
