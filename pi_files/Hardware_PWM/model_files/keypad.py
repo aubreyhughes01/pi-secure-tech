d7478f7b249f67beda4d441677eaf2191274ed5d
Aubrey Hughes <aubreyhughes01@gmail.com>
2024-11-02 23:07:03 -0400

    FIXED encryption/decryption

--- pi_files/Hardware_PWM/model_files/keypad.py
+++ pi_files/Hardware_PWM/model_files/keypad.py
@@ -4,20 +4,23 @@ from cryptography.fernet import Fernet
 
 class Keypad:
     def __init__(self):
-        self.file_path = "Hardware_PWM/model_files/pin.json"
-        self.key_file = "Hardware_PWM/model_files/key.json"
+        self.file_path = "Hardware_PWM/persistence_files/pin.json"
+        self.key_file = "Hardware_PWM/persistence_files/key.key"
         self.create_key()    # Ensure key is created
         self.pin = self.read_passkey()
 
     def read_passkey(self):
-        data = self.decrypt_data()
-        return data["passkey"]
+        data = self.decrypt_data()      # decrypt the json file
+        json_data = json.loads(data.decode("utf-8"))    # convert the data into a python dictionary
+        return json_data["passkey"]
 
     def change_passkey(self, new_key):
         data = self.decrypt_data()
+        json_data = json.loads(data.decode("utf-8"))
 
-        data['passkey'] = new_key
-
+        json_data['passkey'] = new_key
+        data = json.dumps(json_data).encode("utf-8")
+        
         encrypted_data = self.encrypt_data(data)
 
         with open(self.file_path, 'wb') as file:
@@ -26,11 +29,21 @@ class Keypad:
     def create_key(self):
         if not os.path.exists(self.key_file):
             key = Fernet.generate_key()
-            with open(self.key_file,'wb') as file:
+            with open(self.key_file,'wb') as file:  # save key
                 file.write(key)
 
+            with open(self.file_path, "rb") as file:  # read in the json file
+                data = file.read()
+
+            fernet = Fernet(key)
+            encrypted_data = fernet.encrypt(data)   # encrypt the json file
+
+            with open(self.file_path, "wb") as file:   # save the encrypted json file
+                file.write(encrypted_data)
+
+
     def decrypt_data(self):
-        with open(self.key_file,'rb') as file:
+        with open(self.key_file, "rb") as file:
             key = file.read()
         
         with open(self.file_path, 'rb') as file:
@@ -39,14 +52,14 @@ class Keypad:
         fernet = Fernet(key)
         decrypted_data = fernet.decrypt(encrypted_data)
 
-        return json.loads(decrypted_data.decode('utf-8'))  # Decode and load JSON
+        return decrypted_data
 
     def encrypt_data(self, data):
         with open(self.key_file, 'rb') as file:
             key = file.read()
         
         fernet = Fernet(key)
-        json_data = json.dumps(data).encode('utf-8')  # Convert data to JSON bytes
-        encrypted_data = fernet.encrypt(json_data)
+        encrypted_data = fernet.encrypt(data)
 
         return encrypted_data
+
