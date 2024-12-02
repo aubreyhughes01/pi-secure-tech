23b818a8bebae72329b0377097e7614684f87275
Aubrey Hughes <aubreyhughes01@gmail.com>
2024-12-02 11:16:06 -0500

    UPDATED gui and Identity class to implement face id

--- pi_files/Hardware_PWM/gui_files/display.py
+++ pi_files/Hardware_PWM/gui_files/display.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from ..model_files.keypad import Keypad
 from ..model_files.rotator import Rotator
+from ..model_files.identity import Identity
 from time import sleep
 
 root = Tk()
@@ -27,24 +28,30 @@ def lock():
     global operator
     kp = Keypad()
     if text.get() == kp.pin:
-        rt = Rotator(0)
-        rt.rotate()
-        sleep(1)
-        text.set("DOOR IS LOCKED")
+        if scan() == True:
+            rt = Rotator(105)
+            rt.rotate()
+            sleep(1)
+            text.set("DOOR IS LOCKED")
+        else:
+            text.set("FACIAL ID FAILED")
     else:
-        text.set("INVALID")
+        text.set("INVALID PIN")
     operator = ""
 
 def unlock():
     global operator
     kp = Keypad()
     if text.get() == kp.pin:
-        rt = Rotator(105)
-        rt.rotate()
-        sleep(1)
-        text.set("DOOR IS UNLOCKED")
+        if scan() == True:
+            rt = Rotator(105)
+            rt.rotate()
+            sleep(1)
+            text.set("DOOR IS UNLOCKED")
+        else:
+            text.set("FACIAL ID FAILED")
     else:
-        text.set("INVALID")
+        text.set("INVALID PIN")
     operator = ""
     
 def update():
@@ -69,35 +76,60 @@ def is_clicked():
         text.set("Pin Updated Successfully")
     operator = ""
 
-display = Entry(root,bd=15,font=("times new roman",30,"bold"),bg="black",fg="white",justify="center",textvariable=text).grid(columnspan=3, sticky="nsew")
+def configure_id():
+    global operator
+    identity = Identity()
+    kp = Keypad()
+    if text.get() == kp.pin + "0000":
+        identity.capture("Hardware_PWM/persistence_files/database/")
+        text.set("FACE PROFILE ADDED SUCCESSFULLY")
+        operator = ""
+        return True
+    else:
+        text.set("FACE PROFILE CONFIGURATION FAILED")
+        operator = ""
+        return False
+    
+def scan():
+    identity = Identity()
+    rt = Rotator(105)
+    identity.capture("Hardware_PWM/persistence_files/temp/")
+    if identity.compare() == True:
+        return True
+    else:
+        return False    
 
-btn1 = Button(root,command=lambda:click(1),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="1").grid(row=1,column=0, sticky="nsew")
-btn2 = Button(root,command=lambda:click(2),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="2").grid(row=1,column=1, sticky="nsew")
-btn3 = Button(root,command=lambda:click(3),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="3").grid(row=1,column=2, sticky="nsew")
+display = Entry(root,bd=15,font=("times new roman",30,"bold"),bg="black",fg="white",justify="center",textvariable=text).grid(columnspan=4, sticky="nsew")
+
+btn0 = Button(root,command=lambda:click(0),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="0").grid(row=1,column=0, sticky="nsew")
+btn1 = Button(root,command=lambda:click(1),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="1").grid(row=1,column=1, sticky="nsew")
+btn2 = Button(root,command=lambda:click(2),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="2").grid(row=1,column=2, sticky="nsew")
+btn3 = Button(root,command=lambda:click(3),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="3").grid(row=1,column=3, sticky="nsew")
 btn4 = Button(root,command=lambda:click(4),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="4").grid(row=2,column=0, sticky="nsew")
 btn5 = Button(root,command=lambda:click(5),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="5").grid(row=2,column=1, sticky="nsew")
 btn6 = Button(root,command=lambda:click(6),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="6").grid(row=2,column=2, sticky="nsew")
-btn7 = Button(root,command=lambda:click(7),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="7").grid(row=3,column=0, sticky="nsew")
+btn7 = Button(root,command=lambda:click(7),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="7").grid(row=2,column=3, sticky="nsew")
 btn8 = Button(root,command=lambda:click(8),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="8").grid(row=3,column=1, sticky="nsew")
 btn9 = Button(root,command=lambda:click(9),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="9").grid(row=3,column=2, sticky="nsew")
-btn0 = Button(root,command=lambda:click(0),padx=30,pady=30,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="0").grid(row=4,column=1, sticky="nsew")
 
-lockbtn =Button(root,command=lambda:lock(),padx=0,pady=0,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="LOCK").grid(row=4,column=2, sticky="nsew")
-unlockbtn =Button(root,command=lambda:unlock(),padx=0,pady=0,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="UNLOCK").grid(row=5,column=2, sticky="nsew")
+lockbtn =Button(root,command=lambda:lock(),padx=0,pady=0,bg="grey",bd=30,fg="black",font=("times new roman",30,"bold"),text="LOCK").grid(row=3,column=0, sticky="nsew")
+unlockbtn =Button(root,command=lambda:unlock(),padx=0,pady=0,bg="grey",bd=30,fg="black",font=("times new roman",30,"bold"),text="UNLOCK").grid(row=3,column=3, sticky="nsew")
 
-clearbtn =Button(root,command=lambda:clear(),padx=0,pady=0,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="CLEAR").grid(row=5, column=1, sticky="nsew")
+clearbtn =Button(root,command=lambda:clear(),padx=0,pady=0,bg="grey",bd=30,fg="black",font=("times new roman",30,"bold"),text="CLEAR").grid(row=4, column=0, sticky="nsew")
 
-updatebtn = Button(root,command=lambda:update(),padx=0,pady=0,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="UPDATE").grid(row=4, column=0, sticky="nsew")
-changebtn = Button(root,command=lambda:is_clicked(),padx=0,pady=0,bg="black",bd=30,fg="white",font=("times new roman",30,"bold"),text="CHANGE").grid(row=5, column=0, sticky="nsew")
+updatebtn = Button(root,command=lambda:update(),padx=0,pady=0,bg="grey",bd=30,fg="black",font=("times new roman",30,"bold"),text="UPDATE").grid(row=4, column=1, sticky="nsew")
+changebtn = Button(root,command=lambda:is_clicked(),padx=0,pady=0,bg="grey",bd=30,fg="black",font=("times new roman",30,"bold"),text="CHANGE").grid(row=4, column=2, sticky="nsew")
 
-root.grid_rowconfigure(0, weight=1)
+scanbtn = Button(root,command=lambda:configure_id(),padx=30,pady=30,bg="grey",bd=30,fg="black",font=("times new roman",30,"bold"),text="FaceID").grid(row=4,column=3, sticky="nsew")
+
 root.grid_columnconfigure(0, weight=1)
-root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(1, weight=1)
-root.grid_rowconfigure(2, weight=1)
 root.grid_columnconfigure(2, weight=1)
+root.grid_columnconfigure(3, weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_rowconfigure(2, weight=1)
 root.grid_rowconfigure(3, weight=1)
 root.grid_rowconfigure(4, weight=1)
-root.grid_rowconfigure(5, weight=1)
 
 root.mainloop()
--- pi_files/Hardware_PWM/model_files/identity.py
+++ pi_files/Hardware_PWM/model_files/identity.py
@@ -1,13 +1,17 @@
+import os
 import cv2
-import pandas as pd
-from deepface import DeepFace
+import sys
+import time
+#import pandas as pd
+#from deepface import DeepFace
 
 class Identity:
 
     def __init__(self):
-        self.database_path = "database"
+        self.database_path = "Hardware_PWM/persistence_files/database"
+        self.blank_database = "Hardware_PWM/persistence_files/blank_database"
 
-    def capture(self):
+    def capture(self, location):
         cam = cv2.VideoCapture(0)
 
         if not cam.isOpened():
@@ -18,24 +22,30 @@ class Identity:
         ret, frame = cam.read()
 
         if ret:
-            cv2.imwrite("captured_image.jpg", frame)
+            if location == "Hardware_PWM/persistence_files/temp/":
+                cv2.imwrite(location + "captured_image.jpg", frame)
+            else:
+                timestamp = int(time.time() * 1000)  # Current timestamp in milliseconds
+                filename = f"captured_image{timestamp}.jpg"
+                cv2.imwrite(location + filename, frame)
 
         cam.release()
         cv2.destroyAllWindows()
 
     def compare(self):
         # dataframe object
-        df = DeepFace.find(img_path = "captured_image.jpg", db_path = database_path)
+        df = DeepFace.find(img_path = "Hardware_PWM/persistence_files/temp/captured_image.jpg", db_path = database_path)
+        blankdf = DeepFace.find(img_path = "Hardware_PWM/persistence_files/temp/captured_image.jpg", db_path = blank_database)
 
-        if df.empty:
-            print("No match")
+        try:
+            os.remove("Hardware_PWM/persistence_files/temp/captured_image.jpg")
+        except:
+            print("Error deleting temp image")
 
-            return False
-        else:
+        if sys.getsizeof(df) > sys.getsizeof(blankdf):     # comparison of dataframe size when image is identified vs when it's unsuccessful
             print("Match found")
-            print(result)
-
             return True
+        else:
+            print("No match found")
+            return False
 
-
-        
